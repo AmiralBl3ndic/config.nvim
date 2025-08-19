1b7259934a21e494cb0e896a6278abe330ed81f6
AmiralBl3ndic <camille@fungiball.io>
2025-08-19 09:21:41 +0200

    config: disable autochdir + set tab width to 2

--- lua/custom/config_overrides.lua
+++ lua/custom/config_overrides.lua
@@ -1,8 +1,12 @@
--- Always use current opened file as the current working directoy
-vim.o.autochdir = true
+-- Always use current opened file as the current working directoy (if true)
+vim.o.autochdir = false
 
 -- Relative line numbers
 vim.o.relativenumber = true
+
+-- Tab size
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
 
 -- Load all custom Lua code as "fake" plugins
 require 'custom.custom_plugins'
